257fae7d97b7ce036b9e7c6c1c7b224c82b9defe
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-02-05 10:43:37 +0000

    Add Record class
    Add a Record class along with a few tests that check whether the
    attributes of Record instances are set correctly.

---
+++ pvi/record.py
@@ -0,0 +1,7 @@
+class Record(object):
+
+    def __init__(self, prefix, suffix, fields, infos):
+        self.prefix = prefix
+        self.suffix = suffix
+        self.fields = fields
+        self.infos = infos
--- tests/test_pvi.py
+++ tests/test_pvi.py
@@ -1,1 +1,21 @@
 import unittest
+
+from pvi.record import Record
+
+
+class TestRecord(unittest.TestCase):
+
+    def setUp(self):
+        self.record = Record("$(P)$(R)", "ThresholdEnergy", {"PINI": "YES"}, {"autosaveFields": "VAL"})
+
+    def test_prefix_attribute(self):
+        assert self.record.prefix == "$(P)$(R)"
+
+    def test_suffix_attribute(self):
+        assert self.record.suffix == "ThresholdEnergy"
+
+    def test_fields_attribute(self):
+        assert self.record.fields == {"PINI": "YES"}
+
+    def test_infos_attribute(self):
+        assert self.record.infos == {"autosaveFields": "VAL"}
