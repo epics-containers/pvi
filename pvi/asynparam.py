1bc4786866e3f0d946a811bafdbd10d7db01ae8d
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-02-06 17:12:29 +0000

    Add Float64AsynParam class

--- pvi/asynparam.py
+++ pvi/asynparam.py
@@ -9,3 +9,13 @@ class AsynParam(object):
     @property
     def asyntyp(self):
         raise NotImplementedError(self)
+
+
+class Float64AsynParam(AsynParam):
+
+    def __init__(self, name, initial_value):
+        super(Float64AsynParam, self).__init__(name, initial_value)
+
+    @property
+    def asyntyp(self):
+        return "asynParamFloat64"
--- tests/test_asynparam.py
+++ tests/test_asynparam.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pvi.asynparam import AsynParam
+from pvi.asynparam import AsynParam, Float64AsynParam
 
 
 class TestAsynParam(unittest.TestCase):
@@ -17,3 +17,10 @@ class TestAsynParam(unittest.TestCase):
     def test_asyntyp_property(self):
         with self.assertRaises(NotImplementedError):
             self.asynparam.asyntyp
+
+
+class TestFloat64AsynParam(unittest.TestCase):
+
+    def test_asyntyp_property(self):
+        float64_asynparam = Float64AsynParam("THRESHOLDENERGY", 10)
+        assert float64_asynparam.asyntyp == "asynParamFloat64"
