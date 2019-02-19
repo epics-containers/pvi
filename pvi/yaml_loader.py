6336e33514526c014018b1196461f3963b0e121e
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-02-19 14:04:52 +0000

    Add get_component_yaml_info function
    Add function to look at a YAML file (with a format that pvi is
    expecting, ie, it contains a 'components' section) and extract the
    component types and their corresponding argument values.

--- pvi/yaml_loader.py
+++ pvi/yaml_loader.py
@@ -1,8 +1,9 @@
 import importlib
 from annotypes import Any, TYPE_CHECKING
+from ruamel import yaml
 
 if TYPE_CHECKING:
-    from typing import List, Callable
+    from typing import List, Callable, Dict, Tuple
 
 
 def lookup_component(component_type, filename, lineno):
@@ -23,3 +24,22 @@ def lookup_component(component_type, filename, lineno):
               (filename, lineno, pkg, ident))
 
     return ob
+
+
+def get_component_yaml_info(yaml_path):
+    # type: (str) -> List[Tuple[str, str, int, Dict]]
+    with open(yaml_path) as f:
+        text = f.read()
+
+    code = yaml.load(text, Loader=yaml.RoundTripLoader)
+    components_section = code["components"]
+    components_and_info = []
+
+    for component_info in components_section:
+        pos_info = component_info.lc.key("type")
+        lineno = pos_info[0]
+        component_type = component_info.pop("type")
+        components_and_info.append((component_type, yaml_path, lineno,
+                                    component_info))
+
+    return components_and_info
--- tests/test_yaml_loader.py
+++ tests/test_yaml_loader.py
@@ -1,7 +1,38 @@
 import unittest
 from mock import Mock, patch
 
-from pvi.yaml_loader import lookup_component
+from pvi.yaml_loader import lookup_component, get_component_yaml_info
+
+yaml_text = """
+type: pvi.producers.MyProducer
+overridden_by: $(yamlname).local.yaml
+
+takes:
+  - type: builtin.takes.string
+    name: P
+    description: Record prefix part 1
+
+  - type: builtin.takes.string
+    name: R
+    description: Record prefix part 2
+
+  - type: builtin.takes.string
+    name: PORT
+    description: Port name
+
+components:
+  - type: mymodule.components.MyComponent
+    name: SomeName
+    description: Some description
+    prec: 3
+    egu: keV
+    initial_value: 10
+    autosave_fields: VAL
+    demand: Yes
+    readback: Yes
+    widget: TextInput
+    group: AncillaryInformation
+"""
 
 
 class TestLookupComponent(unittest.TestCase):
@@ -21,3 +52,36 @@ class TestLookupComponent(unittest.TestCase):
 
         mock_import.assert_called_once_with("pvi.modules.mymodule.components")
         assert returned_component == component
+
+
+class TestGetComponentYamlInfo(unittest.TestCase):
+
+    def test_get_info_for_one_component(self):
+        filepath = "/tmp/yamltest.yaml"
+
+        with open(filepath, "w") as f:
+            f.write(yaml_text)
+
+        expected_component_type = "mymodule.components.MyComponent"
+        expected_component_type_lineno = 18
+        expected_params = dict(
+            name="SomeName",
+            description="Some description",
+            prec=3,
+            egu="keV",
+            autosave_fields="VAL",
+            widget="TextInput",
+            group="AncillaryInformation",
+            initial_value=10,
+            demand="Yes",
+            readback="Yes"
+        )
+
+        components_and_info = get_component_yaml_info(filepath)
+        returned_component_type = components_and_info[0][0]
+        returned_component_type_lineno = components_and_info[0][2]
+        returned_params = components_and_info[0][3]
+
+        assert returned_component_type == expected_component_type
+        assert returned_component_type_lineno == expected_component_type_lineno
+        assert returned_params == expected_params
