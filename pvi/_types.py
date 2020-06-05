f8ae3a5e48d62de2570a15392f668b0c72799f9e
Emma Dixon <emma.dixon@diamond.ac.uk>
2020-06-05 10:46:50 +0100

    Override dict method for Component
    - .dict() must include type
    - Correctly show the Enum value

--- pvi/_types.py
+++ pvi/_types.py
@@ -1,5 +1,5 @@
 """The types that should be inherited from or produced by Fields."""
-
+from enum import Enum
 from typing import (
     Any,
     Callable,
@@ -77,6 +77,17 @@ class Component(WithType):
         if label is None:
             label = camel_to_title(self.name)
         return label
+
+    # override dict method
+    # want to always return 'type' even when using exclude_unset=True
+    # want to return all enums as values
+    def dict(self, **kwargs):
+        default_export = super().dict(**kwargs)
+        modified_export = {**dict(type=self.type), **default_export}
+        for key, value in modified_export.items():
+            if isinstance(value, Enum):
+                modified_export[key] = value.value
+        return modified_export
 
     @classmethod
     def on_each_node(
