3c6995547b5c6ca346abb5c810a47354cffa5689
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-01-21 10:36:17 +0000

    Add way of regenerating test output

---
+++ tests/regenerate_test_output.sh
@@ -0,0 +1,8 @@
+# If you changed some code which you expect to change the output files, then run
+# this script to regenerate the test output.
+#
+# NOTE: Do not run this script unless you are expecting the output files to
+# change. Examine the git diff carefully to make sure the new test output files
+# are as you expect them to be
+
+cd $(dirname $0)/.. && PVI_REGENERATE_OUTPUT=1 pipenv run tests
