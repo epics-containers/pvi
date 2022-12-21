fe272191496fbb571a2b0ad232fd8d3cfc7704e4
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-12-21 11:50:28 +0000

    Make breakpoint on exception work in vscode

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+
+import pytest
+
+# See: https://github.com/pytest-dev/pytest/issues/7409
+if os.getenv("PYTEST_RAISE", "0") != "0":
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_exception_interact(call):
+        raise call.excinfo.value
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_internalerror(excinfo):
+        raise excinfo.value
