3ffc8ab58206969c569d5b370e732a74f173e318
GDYendell <GDYendell@users.noreply.github.com>
2023-07-14 17:35:27 +0000

    deploy: 1ce9725439110f146e4387e779d3753c2fd87e37

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,14 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4+33.g2a8b27f',
-    LANGUAGE: 'None',
+    VERSION: '0.4+34.g1ce9725',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
