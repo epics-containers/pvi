f062b38f2af2a5b7350092a6a59142c5a2d7ade8
GDYendell <GDYendell@users.noreply.github.com>
2023-07-14 17:42:10 +0000

    deploy: 6b70a6e00ddc6f3679e87e9fc86a2ed7382120b4

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4+34.g1ce9725',
+    VERSION: '0.4+36.g6b70a6e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
