98b47e6e166f00be51cb3ac3e4782a372d88436d
GDYendell <GDYendell@users.noreply.github.com>
2023-07-17 13:28:46 +0000

    deploy: 89b6a04c3b9ee6dc893faa75d51ea8e409230ceb

--- skeleton-update/_static/documentation_options.js
+++ skeleton-update/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.dev37+g5aa67c6',
+    VERSION: '0.5.dev37+g89b6a04',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
