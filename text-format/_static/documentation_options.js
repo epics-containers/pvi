696a0a38d8914a461d89989dd074b7e358c4c77f
GDYendell <GDYendell@users.noreply.github.com>
2023-07-18 12:22:19 +0000

    deploy: 9c179c471c697092250f449564036a596612286c

--- text-format/_static/documentation_options.js
+++ text-format/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.dev38+g921fd66',
+    VERSION: '0.5.dev38+g9c179c4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
