4d6e8b1f780ff48b05f4bc3454e7ff653ff8abb8
GDYendell <GDYendell@users.noreply.github.com>
2023-10-06 17:22:51 +0000

    deploy: 6b72534674e05b201cab93274fc51fb708dd5947

--- device-ref/_static/documentation_options.js
+++ device-ref/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.dev1+gfc8c932',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.6.dev3+g6b72534',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
