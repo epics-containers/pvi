ea36e97fab7234e08a5e37eae809ca39935255c5
GDYendell <GDYendell@users.noreply.github.com>
2023-11-08 10:15:55 +0000

    deploy: 190b27b39326ea8a5d1e9403d05d1d3b4d3d6eba

--- device-ref/_static/documentation_options.js
+++ device-ref/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.6.dev4+g6a51e67',
+    VERSION: '0.6.dev18+g190b27b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
