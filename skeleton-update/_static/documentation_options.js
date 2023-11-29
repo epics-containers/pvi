232f538f7ac86929126d41050bf8434800891171
GDYendell <GDYendell@users.noreply.github.com>
2023-11-29 13:55:40 +0000

    deploy: 711cc7fbe8bd8f6c2aaf150a5f0008abbb2d4a97

---
+++ skeleton-update/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.6.2.dev67+g711cc7f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
