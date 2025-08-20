9ebf4a88b77e378df7a881cce8fd3c2727492f78
James Souter <james.souter@diamond.ac.uk>
2025-08-20 13:20:33 +0000

    Don't draw color_bar on bob images by default

--- src/pvi/_format/bob.py
+++ src/pvi/_format/bob.py
@@ -104,9 +104,10 @@ class BobTemplate(UITemplate[_Element]):
                 add_format(t_copy, BOB_TEXT_FORMATS[TextFormat(format)])
             case ("byte_monitor", BitField() as bit_field):
                 add_byte_number_of_bits(t_copy, bit_field.number_of_bits)
-            case ("image", ImageRead(grayscale=grayscale)):
+            case ("image", ImageRead(grayscale=grayscale, color_bar=color_bar)):
                 if grayscale:
                     set_color_map(t_copy, "GRAY")
+                set_visibility(SubElement(t_copy, "color_bar"), color_bar)
             case _:
                 pass
 
@@ -238,9 +239,13 @@ def add_format(element: _Element, format: str):
     if format:
         SubElement(element, "format").text = format
 
+def set_visibility(element: _Element, visible: bool):
+    SubElement(element, "visible").text = "true" if visible else "false"
+
 def set_color_map(element: _Element, color_map: str):
     color_map_element = SubElement(element, "color_map")
     SubElement(color_map_element, "name").text = color_map
+
 
 def add_byte_number_of_bits(element: _Element, number_of_bits: int):
     SubElement(element, "numBits").text = str(number_of_bits)
